d72cd771cd4600b506094b5aab9285ab7a22752c
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-08-16 14:17:17 +0200

    (mew) server responding to browser

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cookieP = require('cookie-parser');
 
 const app = express();
 
+//cookie check
 
+
+//when users first arrive
+
+app.get('/', (req,res) => {
+    res.redirect('/petition');
+});
 app.get('/petition', (req, res)=> {
-
+    res.send('Hello World');
 });
 
 app.post('/petition', (req, res) => {
