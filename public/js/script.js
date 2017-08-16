1e8f62c9b481850ace9566a9c594760909a2c23e
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-08-16 16:59:10 +0200

    (mew) unhid text field to get database working

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,9 @@
 
 //client side javascript
 //handle canvas jonks
+//
+//handle submit form
+//in the submit form make canvas stuff the value of the hiddent input field.
+//canvasElement.toDataURL
+
 console.log('it is getting to the client');
