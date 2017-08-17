4916de6ade40f5c26c010b72bc1779373861e836
Marguerite Wiseman <margueritewiseman@Marguerites-Air.fritz.box>
2017-08-17 04:05:55 +0200

    (mew) canvas works to show signature

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,37 @@
 
 //client side javascript
 //handle canvas jonks
-//
-//handle submit form
+(function() {
+
+    function signature() {
+        const canvas = document.getElementById('canv');
+        const ctx = canvas.getContext('2d');
+
+        function pointerDown(e) {
+            ctx.beginPath();
+            ctx.moveTo(e.offsetX, e.offsetY);
+            canvas.addEventListener('mousemove', paint);
+        }
+
+        function pointerUp(e) {
+            canvas.removeEventListener('mousemove', paint);
+            paint(e);
+        }
+
+        function paint(e) {
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
+        }
+
+        canvas.addEventListener("mousedown", pointerDown);
+        canvas.addEventListener("mouseup", pointerUp);
+    }
+
+    signature();
+}());
+
+
+
 //in the submit form make canvas stuff the value of the hiddent input field.
 //canvasElement.toDataURL
 
