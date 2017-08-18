acd714ba064f9118a7e3aa52340c8b0f4b7a958f
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-08-18 12:02:49 +0200

    (mew) set up new table

--- dbSetup.sql
+++ dbSetup.sql
@@ -1,21 +1,44 @@
 DROP TABLE IF EXISTS signatures;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE signatures (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    signature TEXT NOT NULL
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE signatures (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    signature TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
 
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Leo', 'Dicap', 'leo@gmail', 'rose');
 
-INSERT INTO signatures (first_name, last_name, signature) VALUES ('Leonardo', 'DiCaprio', 'nonsense text');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Maggie', 'Wiseman', 'mw@gmail', 'ilovepandas');
 
-INSERT INTO signatures (first_name, last_name, signature) VALUES ('Maggie', 'Wiseman', 'more nonsense text');
+INSERT INTO signatures (user_id, first_name, last_name, signature) VALUES ('1', 'Leonardo', 'DiCaprio', 'nonsense text');
 
-SELECT first_name, last_name FROM signatures;
+INSERT INTO signatures (user_id, first_name, last_name, signature) VALUES ('2', 'Maggie', 'Wiseman', 'more nonsense text');
 
-SELECT signature FROM signatures WHERE id = 2;
+SELECT signature FROM signatures WHERE user_id = 1;
+SELECT password FROM users WHERE email = 'leo@gmail';
 
-SELECT COUNT(*) FROM signatures;
+
+
+
+
+
+--SELECT first_name, last_name FROM signatures;
+
+--SELECT signature FROM signatures WHERE id = 2;
+
+--SELECT COUNT(*) FROM signatures;
